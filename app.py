06fb57f82e2fc5f950d1a6da3e81c48715817024
Dustin Joosen <67952109+DustinJoosen@users.noreply.github.com>
2023-02-03 13:13:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import sys
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    version = sys.version
+    return f'Hello, python version: {version}'
